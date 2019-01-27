f8351c4a452783616d582d75658b217ecc5a1f29
Gwatcha <31526638+Gwatcha@users.noreply.github.com>
2019-01-27 08:49:56 -0800

    fix return to map after login

--- controllers/ad.js
+++ controllers/ad.js
@@ -2,29 +2,6 @@ const { promisify } = require('util');
 const passport = require('passport');
 const Ad = require('../models/Ad');
 const User = require('../models/User');
-/**
- * POST /ad
- * Add a study buddy Ad for display on the map.
- */
-exports.postAd = (req, res, next) => {
-    req.assert('location', 'Need location paramter').notEmpty();
-
-    const errors = req.validationErrors();
-    const user = req.user;
-
-    console.log('User posting is:');
-    console.log(req.user);
-
-    const Ad = new Ad({
-        user: req.user,
-        time: req.user
-    });
-
-    if (errors) {
-        req.flash('errors', errors);
-        return res.redirect('/map');
-    }
-};
 
 exports.getFindabuddy = (req, res) => {
     res.render('findabuddy', {
