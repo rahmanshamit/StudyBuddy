16d29445a74922b798a599bdd751230aaf7d2517
Gwatcha <31526638+Gwatcha@users.noreply.github.com>
2019-01-27 01:05:20 -0800

    add Ad model and controller

---
+++ controllers/ad.js
@@ -0,0 +1,28 @@
+const { promisify } = require('util');
+const passport = require('passport');
+const Ad = require('../models/Ad');
+/**
+ * POST /ad
+ * Add a study buddy Ad for display on the map.
+ */
+exports.postAd = (req, res, next) => {
+    req.assert('location', 'Need location paramter').notEmpty();
+
+    const errors = req.validationErrors();
+    const user = req.user;
+
+    console.log('User posting is:');
+    console.log(req.user);
+
+    const Ad = new Ad({
+        user: req.user,
+        time: req.user
+    })
+
+    if (errors) {
+        req.flash('errors', errors);
+        return res.redirect('/map');
+    }
+
+    })(req, res, next);
+};
